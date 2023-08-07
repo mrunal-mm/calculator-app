ff7780dd706fae9ee3ed953047e4f008ab17ca62
mrunal-mm <mrunalmm2@gmail.com>
2023-08-07 17:03:28 +0530

    Add displayToScreen function The function displays the user input to the calculator screen.

--- script.js
+++ script.js
@@ -3,6 +3,35 @@ function clearScreen(){
     secondaryScreen.textContent = "00";
 }
 
-const clearButton = document.querySelector('.clear');
+function displayToScreen(e){
+    let buttonClicked = e.target.textContent;
+    let buttonClassName = e.srcElement.className;
+    console.log(buttonClassName)
+    if (primaryScreen.textContent === "00"){
+        if (buttonClassName != "numbers"){
+            primaryScreen.textContent = "00";
+        }
+        else{
+            primaryScreen.textContent = buttonClicked;
+        }
+    }
+    else primaryScreen.textContent += buttonClicked;
+}
+
+function deleteFromScreen(){
+    let newContent = primaryScreen.textContent.slice(0,-1);
+    primaryScreen.textContent = newContent;
+}
+
 const primaryScreen = document.querySelector('.primary-screen');
 const secondaryScreen = document.querySelector('.secondary-screen');
+
+const numberButtons = document.querySelectorAll('.numbers');
+const operatorButtons = document.querySelectorAll('.operator');
+
+const clearButton = document.querySelector('.clear');
+const deleteButton = document.querySelector('.delete');
+
+numberButtons.forEach((button) => button.addEventListener('click', displayToScreen))
+operatorButtons.forEach((button) => button.addEventListener('click', displayToScreen))
+deleteButton.addEventListener('click', deleteFromScreen)
